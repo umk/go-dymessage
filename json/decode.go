037fad1801f97ef639e72dd97a48b5607a76ae88
Uladzislau Maslakou <dreamer.mas@gmail.com>
2019-01-16 09:55:45 +0300

    1

--- json/decode.go
+++ json/decode.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"reflect"
 	"strconv"
 
@@ -14,10 +15,14 @@ import (
 	"github.com/umk/go-dymessage/internal/helpers"
 )
 
-// DecodeNew transforms the JSON representation of the message to a dynamic entity
-// against the provided message definition.
-func (s *Encoder) DecodeNew(b []byte, pd *MessageDef) (*Entity, error) {
-	return s.Decode(b, pd, pd.NewEntity())
+type decoder struct {
+	json *json.Decoder
+}
+
+// DecodeNew transforms the JSON representation of the message to a dynamic
+// entity against the provided message definition.
+func DecodeNew(b []byte, pd *MessageDef) (*Entity, error) {
+	return Decode(b, pd, pd.NewEntity())
 }
 
 // Decode transforms the JSON representation of the message to specified
@@ -27,6 +32,20 @@ func (s *Encoder) DecodeNew(b []byte, pd *MessageDef) (*Entity, error) {
 //
 // If the entity type doesn't correspond the data type of the message
 // definition, the method will panic.
+func Decode(b []byte, pd *MessageDef, e *Entity) (*Entity, error) {
+	buf := bytes.NewBuffer(b)
+	dc := decoder{
+		json: json.NewDecoder(buf),
+	}
+	dc.json.UseNumber()
+	err := dc.decode(b, pd, e)
+	return e, err
+}
+
+func (dc *decoder) decode(b []byte, pd *MessageDef, e *Entity) error {
+
+}
+
 func (s *Encoder) Decode(b []byte, pd *MessageDef, e *Entity) (*Entity, error) {
 	helpers.DataTypesMustMatch(e, pd)
 	var data map[string]interface{}
@@ -42,7 +61,7 @@ func (s *Encoder) Decode(b []byte, pd *MessageDef, e *Entity) (*Entity, error) {
 }
 
 func (s *Encoder) setJsonFields(
-	e *Entity, pd *MessageDef, data map[string]interface{}) error {
+    e *Entity, pd *MessageDef, data map[string]interface{}) error {
 	for _, f := range pd.Fields {
 		value, ok := data[f.Name]
 		if !ok {
@@ -92,7 +111,7 @@ func (s *Encoder) decodeJsonValue(e *Entity, f *MessageFieldDef, value interface
 }
 
 func (s *Encoder) decodeJsonRef(
-	e *Entity, pd *MessageDef, f *MessageFieldDef, value interface{}) error {
+    e *Entity, pd *MessageDef, f *MessageFieldDef, value interface{}) error {
 	if f.Repeated {
 		if items, ok := value.([]interface{}); ok {
 			f.Reserve(e, len(items))
@@ -117,7 +136,7 @@ func (s *Encoder) decodeJsonRef(
 }
 
 func (s *Encoder) decodeEntity(
-	value interface{}, pd *MessageDef, f *MessageFieldDef) (Reference, error) {
+    value interface{}, pd *MessageDef, f *MessageFieldDef) (Reference, error) {
 	switch value := value.(type) {
 	case string:
 		switch f.DataType {
@@ -174,3 +193,165 @@ func (*Encoder) decodePrimitive(value interface{}, f *MessageFieldDef) (Primitiv
 	err := fmt.Errorf("unexpected value %q of %v", value, reflect.TypeOf(value))
 	return GetDefaultPrimitive(), err
 }
+
+
+//// DecodeNew transforms the JSON representation of the message to a dynamic entity
+//// against the provided message definition.
+//func (s *Encoder) DecodeNew(b []byte, pd *MessageDef) (*Entity, error) {
+//	return s.Decode(b, pd, pd.NewEntity())
+//}
+//
+//// Decode transforms the JSON representation of the message to specified
+//// dynamic entity against the provided message definition. The returned entity
+//// is the one that has been provided as an input parameter e, but now populated
+//// with the data.
+////
+//// If the entity type doesn't correspond the data type of the message
+//// definition, the method will panic.
+//func (s *Encoder) Decode(b []byte, pd *MessageDef, e *Entity) (*Entity, error) {
+//	helpers.DataTypesMustMatch(e, pd)
+//	var data map[string]interface{}
+//	decoder := json.NewDecoder(bytes.NewReader(b))
+//	decoder.UseNumber()
+//	if err := decoder.Decode(&data); err != nil {
+//		return nil, err
+//	}
+//	if err := s.setJsonFields(e, pd, data); err != nil {
+//		return nil, err
+//	}
+//	return e, nil
+//}
+//
+//func (s *Encoder) setJsonFields(
+//	e *Entity, pd *MessageDef, data map[string]interface{}) error {
+//	for _, f := range pd.Fields {
+//		value, ok := data[f.Name]
+//		if !ok {
+//			if !s.IgnoreUnknown {
+//				message := fmt.Sprintf("unknown field name %q in the message", f.Name)
+//				return errors.New(message)
+//			}
+//			continue
+//		}
+//		if value != nil {
+//			var err error
+//			if f.DataType.IsRefType() {
+//				err = s.decodeJsonRef(e, pd, f, value)
+//			} else {
+//				err = s.decodeJsonValue(e, f, value)
+//			}
+//			if err != nil {
+//				return err
+//			}
+//		}
+//	}
+//	return nil
+//}
+//
+//func (s *Encoder) decodeJsonValue(e *Entity, f *MessageFieldDef, value interface{}) error {
+//	if f.Repeated {
+//		if items, ok := value.([]interface{}); ok {
+//			f.Reserve(e, len(items))
+//			for i, item := range items {
+//				if value, err := s.decodePrimitive(item, f); err == nil {
+//					f.SetPrimitiveAt(e, i, value)
+//				} else {
+//					return err
+//				}
+//			}
+//		} else {
+//			return fmt.Errorf("expected array, but %q provided", value)
+//		}
+//	} else {
+//		if value, err := s.decodePrimitive(value, f); err == nil {
+//			f.SetPrimitive(e, value)
+//		} else {
+//			return err
+//		}
+//	}
+//	return nil
+//}
+//
+//func (s *Encoder) decodeJsonRef(
+//	e *Entity, pd *MessageDef, f *MessageFieldDef, value interface{}) error {
+//	if f.Repeated {
+//		if items, ok := value.([]interface{}); ok {
+//			f.Reserve(e, len(items))
+//			for i, item := range items {
+//				if value, err := s.decodeEntity(item, pd, f); err == nil {
+//					f.SetReferenceAt(e, i, value)
+//				} else {
+//					return err
+//				}
+//			}
+//		} else {
+//			return fmt.Errorf("expected array, but %q provided", value)
+//		}
+//	} else {
+//		if value, err := s.decodeEntity(value, pd, f); err == nil {
+//			f.SetReference(e, value)
+//		} else {
+//			return err
+//		}
+//	}
+//	return nil
+//}
+//
+//func (s *Encoder) decodeEntity(
+//	value interface{}, pd *MessageDef, f *MessageFieldDef) (Reference, error) {
+//	switch value := value.(type) {
+//	case string:
+//		switch f.DataType {
+//		case DtBytes:
+//			if b, err := base64.StdEncoding.DecodeString(value); err == nil {
+//				return FromBytes(b, false), nil
+//			} else {
+//				return GetDefaultReference(), err
+//			}
+//		case DtString:
+//			return FromString(value), nil
+//		}
+//	case map[string]interface{}:
+//		def := pd.Registry.GetMessageDef(f.DataType)
+//		e := def.NewEntity()
+//		if err := s.setJsonFields(e, pd, value); err != nil {
+//			return GetDefaultReference(), err
+//		}
+//		return FromEntity(e), nil
+//	}
+//	err := fmt.Errorf("unexpected value %q of %v", value, reflect.TypeOf(value))
+//	return GetDefaultReference(), err
+//}
+//
+//func (*Encoder) decodePrimitive(value interface{}, f *MessageFieldDef) (Primitive, error) {
+//	switch value := value.(type) {
+//	case json.Number:
+//		str := string(value)
+//		switch f.DataType {
+//		case DtInt32:
+//			value, err := strconv.ParseInt(str, 10, 32)
+//			return FromInt32(int32(value)), err
+//		case DtInt64:
+//			value, err := strconv.ParseInt(str, 10, 64)
+//			return FromInt64(value), err
+//		case DtUint32:
+//			value, err := strconv.ParseUint(str, 10, 32)
+//			return FromUint32(uint32(value)), err
+//		case DtUint64:
+//			value, err := strconv.ParseUint(str, 10, 64)
+//			return FromUint64(value), err
+//		case DtFloat32:
+//			value, err := strconv.ParseFloat(str, 32)
+//			return FromFloat32(float32(value)), err
+//		case DtFloat64:
+//			value, err := strconv.ParseFloat(str, 64)
+//			return FromFloat64(value), err
+//		}
+//	case bool:
+//		if f.DataType == DtBool {
+//			return FromBool(value), nil
+//		}
+//	}
+//	err := fmt.Errorf("unexpected value %q of %v", value, reflect.TypeOf(value))
+//	return GetDefaultPrimitive(), err
+//}
