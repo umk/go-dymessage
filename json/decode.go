6bce3b99a60dcb154a5665e95590e16ec3e99599
Uladzislau Maslakou <dreamer.mas@gmail.com>
2019-01-17 09:59:47 +0300

    1

--- json/decode.go
+++ json/decode.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
-	"io"
 	"reflect"
 	"strconv"
 
@@ -38,12 +37,175 @@ func Decode(b []byte, pd *MessageDef, e *Entity) (*Entity, error) {
 		json: json.NewDecoder(buf),
 	}
 	dc.json.UseNumber()
-	err := dc.decode(b, pd, e)
+	err := dc.decode(pd, e)
 	return e, err
 }
 
-func (dc *decoder) decode(b []byte, pd *MessageDef, e *Entity) error {
+func (dc *decoder) decode(pd *MessageDef, e *Entity) error {
+	if err := dc.acceptToken('{'); err != nil {
+		return err
+	}
+	for dc.json.More() {
+		t, err := dc.json.Token()
+		if err != nil {
+			return err
+		}
+		name := t.(string)
+		f, ok := pd.TryGetFieldByName(name)
+		if !ok {
+			continue
+		}
+		if f.Repeated {
+			if err := dc.acceptToken('['); err != nil {
+				return err
+			}
+			if f.DataType.IsRefType() {
+				err = dc.decodeJsonRefs()
+			} else {
+				err = dc.decodeJsonValues()
+			}
+			if err := dc.acceptToken(']'); err != nil {
+				return err
+			}
+		} else {
 
+		}
+	}
+	if err := dc.acceptToken('}'); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (dc *decoder) decodeJsonValue(f *MessageFieldDef) (pr Primitive, err error) {
+	switch f.DataType {
+	case DtBool:
+		if b, err := dc.acceptBoolean(); err != nil {
+			return pr, err
+		} else {
+			return FromBool(b), nil
+		}
+	case DtInt32, DtInt64, DtUint32, DtUint64:
+		if n, err := dc.acceptNumber(); err != nil {
+			return pr, err
+		} else if i, err := n.Int64(); err != nil  {
+			return pr, err
+		} else {
+			return FromInt64(i), nil
+		}
+	case DtFloat32, DtFloat64:
+		if n, err := dc.acceptNumber(); err != nil {
+			return pr, err
+		} else if f, err := n.Float64(); err != nil {
+			return pr, err
+		} else {
+			return FromFloat64(f), nil
+		}
+	default:
+		panic(f.DataType)
+	}
+	return
+}
+
+//func (dc *decoder) decodeJsonRefs() error {
+//
+//}
+//
+//func (dc *decoder) decodeJsonValues() error {
+//
+//}
+
+//func (dc *decoder) decodeJsonRefs(e *Entity, pd *MessageDef, f *MessageFieldDef) error {
+//	if f.Repeated {
+//		if items, ok := value.([]interface{}); ok {
+//			f.Reserve(e, len(items))
+//			for i, item := range items {
+//				if value, err := s.decodeEntity(item, pd, f); err == nil {
+//					f.SetReferenceAt(e, i, value)
+//				} else {
+//					return err
+//				}
+//			}
+//		} else {
+//			return fmt.Errorf("expected array, but %q provided", value)
+//		}
+//	} else {
+//		if value, err := s.decodeEntity(value, pd, f); err == nil {
+//			f.SetReference(e, value)
+//		} else {
+//			return err
+//		}
+//	}
+//	return nil
+//}
+//
+//func (dc *decoder) decodeJsonValue(e *Entity, f *MessageFieldDef) error {
+//	if f.Repeated {
+//		if items, ok := value.([]interface{}); ok {
+//			f.Reserve(e, len(items))
+//			for i, item := range items {
+//				if value, err := s.decodePrimitive(item, f); err == nil {
+//					f.SetPrimitiveAt(e, i, value)
+//				} else {
+//					return err
+//				}
+//			}
+//		} else {
+//			return fmt.Errorf("expected array, but %q provided", value)
+//		}
+//	} else {
+//		if value, err := s.decodePrimitive(value, f); err == nil {
+//			f.SetPrimitive(e, value)
+//		} else {
+//			return err
+//		}
+//	}
+//	return nil
+//}
+
+// -----------------------------------------------------------------------------
+// Helper methods
+
+func (dc *decoder) acceptToken(token rune) error {
+	if tok, err := dc.json.Token(); err != nil {
+		return err
+	} else if tok != token {
+		return fmt.Errorf("dymessage: expected token %q, but found %q", token, tok)
+	}
+	return nil
+}
+
+func (dc *decoder) acceptNumber() (json.Number, error) {
+	if tok, err := dc.json.Token(); err != nil {
+		return json.Number(0), err
+	} else if n, ok := tok.(json.Number); !ok {
+		err := fmt.Errorf("dymessage: expected a number, but found %q", tok)
+		return n, err
+	} else {
+		return n, nil
+	}
+}
+
+func (dc *decoder) acceptBoolean() (bool, error) {
+	if tok, err := dc.json.Token(); err != nil {
+		return false, err
+	} else if b, ok := tok.(bool); !ok {
+		err := fmt.Errorf("dymessage: expected a boolean, but found %q", tok)
+		return b, err
+	} else {
+		return b, nil
+	}
+}
+
+func (dc *decoder) acceptString() (string, error) {
+	if tok, err := dc.json.Token(); err != nil {
+		return "", err
+	} else if str, ok := tok.(string); !ok {
+		err := fmt.Errorf("dymessage: expected a string, but found %q", tok)
+		return str, err
+	} else {
+		return str, nil
+	}
 }
 
 func (s *Encoder) Decode(b []byte, pd *MessageDef, e *Entity) (*Entity, error) {
@@ -73,63 +235,7 @@ func (s *Encoder) setJsonFields(
 		}
 		if value != nil {
 			var err error
-			if f.DataType.IsRefType() {
-				err = s.decodeJsonRef(e, pd, f, value)
-			} else {
-				err = s.decodeJsonValue(e, f, value)
-			}
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
 
-func (s *Encoder) decodeJsonValue(e *Entity, f *MessageFieldDef, value interface{}) error {
-	if f.Repeated {
-		if items, ok := value.([]interface{}); ok {
-			f.Reserve(e, len(items))
-			for i, item := range items {
-				if value, err := s.decodePrimitive(item, f); err == nil {
-					f.SetPrimitiveAt(e, i, value)
-				} else {
-					return err
-				}
-			}
-		} else {
-			return fmt.Errorf("expected array, but %q provided", value)
-		}
-	} else {
-		if value, err := s.decodePrimitive(value, f); err == nil {
-			f.SetPrimitive(e, value)
-		} else {
-			return err
-		}
-	}
-	return nil
-}
-
-func (s *Encoder) decodeJsonRef(
-    e *Entity, pd *MessageDef, f *MessageFieldDef, value interface{}) error {
-	if f.Repeated {
-		if items, ok := value.([]interface{}); ok {
-			f.Reserve(e, len(items))
-			for i, item := range items {
-				if value, err := s.decodeEntity(item, pd, f); err == nil {
-					f.SetReferenceAt(e, i, value)
-				} else {
-					return err
-				}
-			}
-		} else {
-			return fmt.Errorf("expected array, but %q provided", value)
-		}
-	} else {
-		if value, err := s.decodeEntity(value, pd, f); err == nil {
-			f.SetReference(e, value)
-		} else {
-			return err
 		}
 	}
 	return nil
@@ -236,7 +342,7 @@ func (*Encoder) decodePrimitive(value interface{}, f *MessageFieldDef) (Primitiv
 //		if value != nil {
 //			var err error
 //			if f.DataType.IsRefType() {
-//				err = s.decodeJsonRef(e, pd, f, value)
+//				err = s.decodeJsonRefs(e, pd, f, value)
 //			} else {
 //				err = s.decodeJsonValue(e, f, value)
 //			}
@@ -272,7 +378,7 @@ func (*Encoder) decodePrimitive(value interface{}, f *MessageFieldDef) (Primitiv
 //	return nil
 //}
 //
-//func (s *Encoder) decodeJsonRef(
+//func (s *Encoder) decodeJsonRefs(
 //	e *Entity, pd *MessageDef, f *MessageFieldDef, value interface{}) error {
 //	if f.Repeated {
 //		if items, ok := value.([]interface{}); ok {
